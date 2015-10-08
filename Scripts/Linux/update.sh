be119729895b1b84363f926a87d0b189dd61ea45
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-08 16:24:40 +0200

    Fixing linux update scripts for space in password

--- Scripts/Linux/update.sh
+++ Scripts/Linux/update.sh
@@ -30,5 +30,5 @@ if [ ! -f "steamcmd.sh" ]; then
 	rm steamcmd_linux.tar.gz
 fi
 
-./steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir ../unturned +app_update 304930 validate +exit
+./steamcmd.sh +@sSteamCmdForcePlatformBitness 32 +login "$STEAM_USERNAME" "$STEAM_PASSWORD" +force_install_dir ../unturned +app_update 304930 validate +exit
 # The Unturned 64 bit build seems to not work for some reason...
