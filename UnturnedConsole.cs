df1b5644e285624c1c31120ab3ffec46991ce768
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-30 23:16:27 +0100

    ...

--- UnturnedConsole.cs
+++ UnturnedConsole.cs
@@ -15,7 +15,7 @@ namespace Rocket.Unturned
             try
             {
                 Logger.Log("A");
-                fileStream = new FileStream(String.Format(Environment.ConsoleFile, Provider.InstanceName), FileMode.Create,FileAccess.Write,FileShare.ReadWrite);
+                fileStream = new FileStream(String.Format(Environment.ConsoleFile, U.Instance.InstanceId), FileMode.Create,FileAccess.Write,FileShare.ReadWrite);
 
                 Logger.Log("B");
                 StreamWriter streamWriter = new StreamWriter(fileStream, System.Text.Encoding.ASCII)
