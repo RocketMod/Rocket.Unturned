d91e46478144c04783350c289a417a2dfc71e388
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-30 22:34:09 +0100

    Update update.sh

--- Scripts/Linux/update.sh
+++ Scripts/Linux/update.sh
@@ -4,17 +4,6 @@
 # Author: fr34kyn01535
 # Note: To make sure Steam Guard is not bugging you better create a new Steam account and disable Steam Guard
 
-# Before you install the Unturned 3 server on linux make sure the following packages are installed:
-
-#apt-get install screen htop unzip                    				     # Utils
-#apt-get install libmono2.0-cil mono-runtime          				     # Mono                                           
-#apt-get install Xorg xinit x11-common                			             # Headless X server
-#apt-get install libglu1-mesa libxcursor1 libxrandr2  			             # Native 64 bit Unity 3D prerequisites	
-#apt-get install libc6:i386 libgl1-mesa-glx:i386 libxcursor1:i386 libxrandr2:i386    # 32 bit prerequisites for Unity 3D
-
-#Unity 3D requires a xserver to run, start it with:
-#screen -S XServer startx
-
 STEAM_USERNAME=$1
 STEAM_PASSWORD=$2
 STEAMCMD_HOME="./steamcmd"
