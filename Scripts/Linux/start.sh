897506c76a044fa66dec668d72d3949a4c34e4d4
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-30 22:33:34 +0100

    Update start.sh

--- Scripts/Linux/start.sh
+++ Scripts/Linux/start.sh
@@ -3,27 +3,12 @@
 # Syntax: start.sh <instance name>
 # Author: fr34kyn01535
 
-#Unity 3D requires a xserver to run, open it with startx in another screen
 export DISPLAY=:0.0
 
-
 INSTANCE_NAME=$1
-UNTURNED_HOME="./unturned/"
-LAUNCHER="RocketLauncher.exe"
+UNTURNED_HOME="./unturned"
 
-if [ ! -f $UNTURNED_HOME$LAUNCHER ]; then #Well, somebody can't configure bash scripts...
-	if [ -f ../../../install_304930.vdf ]; then 
-		UNTURNED_HOME="../../../"
-		if [ ! -f $UNTURNED_HOME$LAUNCHER ]; then
-			if [ -f RocketLauncher ]; then
-				mv RocketLauncher $UNTURNED_HOME$LAUNCHER
-			fi
-		fi
-	fi
-fi
-
-
-if [ -f $UNTURNED_HOME$LAUNCHER ]; then
+if [ -f $UNTURNED_HOME"/RocketLauncher.exe" ]; then
 	cd $UNTURNED_HOME
 	ulimit -n 2048
 	mono RocketLauncher.exe $INSTANCE_NAME
