03f1cc41d531046cf118414c4f7b0b958ddd9b3a
fr34kyn01535 <fr34kyn01535@bam.yt>
2015-10-31 13:25:50 +0100

    Update start.sh

--- Scripts/Linux/start.sh
+++ Scripts/Linux/start.sh
@@ -6,13 +6,38 @@
 export DISPLAY=:0.0
 
 INSTANCE_NAME=$1
-UNTURNED_HOME="./unturned"
+UNTURNED_HOME="./unturned/"
+
+
+YELLLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+printf "XServer: "
+if ! screen -list | grep -q "XServer"; then
+    screen -dmS XServer startx
+	printf "${YELLLOW}STARTING${NC}\n"
+	sleep 3
+else
+	printf "${GREEN}RUNNING${NC}\n"
+fi
+
+printf "Steam: "
+if ! screen -list | grep -q "Steam"; then
+	screen -dmS Steam bash -c "export DISPLAY=:0.0 ; steam"
+	printf "${YELLLOW}STARTING${NC}\n"
+    sleep 10
+else
+	printf "${GREEN}RUNNING${NC}\n"
+fi
+
+echo ""
 
 cd $UNTURNED_HOME
 
-if [ -f "./RocketLauncher.exe" ]; then
+if [ -f RocketLauncher.exe ]; then
 	ulimit -n 2048
 	mono RocketLauncher.exe $INSTANCE_NAME
 else
-	echo "RocketLauncher not found"
+	echo "RocketLauncher not found."
 fi
