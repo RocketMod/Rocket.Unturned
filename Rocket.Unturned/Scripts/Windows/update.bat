3f6cf9c9b6fe03ac08eb6a777feb2c87eea69437
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-03-11 18:32:16 +0100

    Update update.bat

--- Rocket.Unturned/Scripts/Windows/update.bat
+++ Rocket.Unturned/Scripts/Windows/update.bat
@@ -7,15 +7,15 @@ SET HOME=%1
 IF [%1]==[] SET HOME=..\
 SET UNTURNEDHOME=%HOME%Unturned
 SET STEAMHOME=%HOME%Steam\
-
 ECHO Steam directory: %STEAMHOME%
 
-IF NOT EXIST "%STEAMHOME%" (
+IF NOT EXIST "%STEAMHOME%"\steamcmd.zip (
 ECHO Installing SteamCMD into Steam directory
 MKDIR "%STEAMHOME%"
 Powershell -c "Invoke-WebRequest -OutFile 'steamcmd.zip' 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip'"
-Powershell -c "Expand-Archive 'steamcmd.zip' -Destination '%STEAMHOME%'"
+CALL :unzip "%~dp0" "%~dp0steamcmd.zip"
 DEL steamcmd.zip
+MOVE steamcmd.exe %STEAMHOME%
 )
 
 CD "%STEAMHOME%"
@@ -27,3 +27,20 @@ IF EXIST "%HOME%\Assembly-CSharp.dll" COPY "%HOME%\Assembly-CSharp.dll" "%UNTURN
 IF EXIST "%HOME%\Rocket.API.dll" COPY "%HOME%\Rocket.API.dll" "%UNTURNEDHOME%\Unturned_Data\Managed"
 IF EXIST "%HOME%\Rocket.Core.dll" COPY "%HOME%\Rocket.Core.dll" "%UNTURNEDHOME%\Unturned_Data\Managed"
 IF EXIST "%HOME%\Rocket.Unturned.dll" COPY "%HOME%\Rocket.Unturned.dll" "%UNTURNEDHOME%\Unturned_Data\Managed"
+
+exit /b
+
+:unzip <destination> <archive>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
