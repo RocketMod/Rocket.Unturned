f7ff31606e8725b9178b9e8ff31652bc775314fe
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-05-06 17:40:38 +0200

    Update UnturnedPlayerEvents.cs

--- Rocket.Unturned/Events/UnturnedPlayerEvents.cs
+++ Rocket.Unturned/Events/UnturnedPlayerEvents.cs
@@ -137,7 +137,7 @@ namespace Rocket.Unturned.Events
             }
             catch (Exception ex)
             {
-                Logger.Log(ex,"Failed to receive packet \""+W+"\"");
+                Logger.LogException(ex,"Failed to receive packet \""+W+"\"");
             }
         }
 
