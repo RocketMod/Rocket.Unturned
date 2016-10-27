5d6b9a1aaecea944d686f74416b72bca33bb1dbd
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-10-27 23:50:56 +0200

    Changed contains to StartsWith & EndsWith

--- Rocket.Unturned/Permissions/UnturnedPermissions.cs
+++ Rocket.Unturned/Permissions/UnturnedPermissions.cs
@@ -67,7 +67,7 @@ namespace Rocket.Unturned.Permissions
                     SteamPending steamPending = Provider.pending.FirstOrDefault(x => x.playerID.CSteamID == r.m_SteamID);
                     if (steamPending != null)
                     {
-                        if (!steamPending.playerID.CharacterName.Contains(g.Prefix) && !steamPending.playerID.CharacterName.Contains(g.Suffix))
+                        if (!steamPending.playerID.CharacterName.StartsWith(g.Prefix) && !steamPending.playerID.CharacterName.EndsWith(g.Suffix))
                         {
                             steamPending.playerID.CharacterName = g.Prefix + steamPending.playerID.CharacterName + g.Suffix;
                         }
