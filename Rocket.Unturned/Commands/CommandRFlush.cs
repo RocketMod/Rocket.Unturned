38ade6d932cf53d309f50774b69020ed9a0b2c44
Travis Greenman <cartman-2000@cartman-2000.net>
2018-03-18 01:01:07 -0700

    Adds Commands for RCON info/control.

---
+++ Rocket.Unturned/Commands/CommandRFlush.cs
@@ -0,0 +1,66 @@
+﻿using Rocket.API;
+using Rocket.Core.RCON;
+using Rocket.Unturned.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rocket.Unturned.Commands
+{
+    class CommandRFlush : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Console; }
+        }
+
+        public string Help
+        {
+            get { return "Kicks all RCON clients off of the server."; }
+        }
+
+        public string Name
+        {
+            get { return "rflush"; }
+        }
+
+        public List<string> Permissions
+        {
+            get { return new List<string>() { "rocket.rflush" }; }
+        }
+
+        public string Syntax
+        {
+            get { return ""; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length == 0 || command.Length > 1 || command[0] != "y")
+            {
+                UnturnedChat.Say(caller, U.Translate("command_rflush_help"));
+            }
+            else
+            {
+                UnturnedChat.Say(caller, U.Translate("command_rflush_total", RCONServer.Clients.Count.ToString()));
+                List<RCONConnection> connections = new List<RCONConnection>();
+                connections.AddRange(RCONServer.Clients);
+                for (int i = 0; i < connections.Count; i++)
+                {
+                    RCONConnection client = connections[i];
+                    if (client.Client.Client.Connected)
+                    {
+                        UnturnedChat.Say(caller, U.Translate("command_rflush_line", i + 1, client.InstanceID, client.Address));
+                        client.Close();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rocket.Unturned/Commands/CommandRKick.cs
@@ -0,0 +1,64 @@
+﻿using Rocket.API;
+using Rocket.API.Extensions;
+using Rocket.Core.RCON;
+using Rocket.Unturned.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rocket.Unturned.Commands
+{
+    class CommandRKick : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Console; }
+        }
+
+        public string Help
+        {
+            get { return "Kicks a client off of RCON."; }
+        }
+
+        public string Name
+        {
+            get { return "rkick"; }
+        }
+
+        public List<string> Permissions
+        {
+            get { return new List<string>() { "rocket.rkick" }; }
+        }
+
+        public string Syntax
+        {
+            get { return "<ConnectionID>"; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            int? instance = command.GetInt32Parameter(0);
+            if (command.Length == 0 || command.Length > 1 || instance == null)
+            {
+                UnturnedChat.Say(caller, U.Translate("command_rkick_help"));
+                return;
+            }
+            foreach (RCONConnection client in RCONServer.Clients)
+            {
+                if (client.InstanceID == (int)instance)
+                {
+                    UnturnedChat.Say(caller, U.Translate("command_rkick_kicked", instance.ToString(), client.Address));
+                    client.Close();
+                    return;
+                }
+            }
+            UnturnedChat.Say(caller, U.Translate("command_rkick_notfound", instance.ToString()));
+        }
+    }
+}
