017033159eff4774bcdbe7e1fc3bf2228f2e7961
Sven Mawby <fr34kyn01535@bam.yt>
2018-05-09 20:43:32 +0200

    ..

--- Rocket.Unturned/UnturnedImplementation.cs
+++ Rocket.Unturned/UnturnedImplementation.cs
@@ -85,7 +85,8 @@ namespace Rocket.Unturned
 
         private void OnServerShutdown()
         {
-            runtime.Shutdown();
+            //runtime.Shutdown();
+            //Todo: Add Shutdown to IRuntime
         }
 
         private void OnPlayerDamaged(SDG.Unturned.Player uPlayer, ref EDeathCause cause, ref ELimb limb, ref CSteamID killerId, ref Vector3 direction, ref float damage, ref float times, ref bool canDamage)
