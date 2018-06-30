4e4f41707ba4f155483a163d590995d2a0cd00b9
Trojaner_ <trojaner25@gmail.com>
2018-06-30 05:23:21 +0300

    Added Tls Workaround

--- Rocket.Unturned/RocketUnturnedHost.cs
+++ Rocket.Unturned/RocketUnturnedHost.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using Rocket.API;
 using Rocket.API.Commands;
 using Rocket.API.DependencyInjection;
@@ -56,6 +59,8 @@ namespace Rocket.Unturned
 
         public void Init(IRuntime runtime)
         {
+            ApplyTlsWorkaround();
+
             this.runtime = runtime;
             rocketGameObject = new GameObject();
             Object.DontDestroyOnLoad(rocketGameObject);
@@ -102,6 +107,37 @@ namespace Rocket.Unturned
 
             CommandWindow.onCommandWindowOutputted += (text, color)
                 => logger.LogNative(text?.ToString());
+        }
+
+        private void ApplyTlsWorkaround()
+        {
+            //http://answers.unity.com/answers/1089592/view.html
+            ServicePointManager.ServerCertificateValidationCallback = CertificateValidationWorkaroundCallback;
+        }
+
+        public bool CertificateValidationWorkaroundCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None)
+            {
+                foreach (X509ChainStatus chainStatus in chain.ChainStatus)
+                {
+                    if (chainStatus.Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                        chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                        chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                        chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                        bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                        if (!chainIsValid)
+                        {
+                            isOk = false;
+                        }
+                    }
+                }
+            }
+            return isOk;
         }
 
         private void OnServerShutdown()
