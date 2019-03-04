9743187fad64a0d31de38a59fb80d7c7f36f8797
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-03-04 19:23:52 +0300

    Fix bootstrap errors

--- Rocket.Unturned.Module/RocketUnturnedModule.cs
+++ Rocket.Unturned.Module/RocketUnturnedModule.cs
@@ -1,26 +1,45 @@
 ﻿using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
+using NuGet.Common;
 using SDG.Framework.Modules;
 using SDG.Unturned;
+using Debug = UnityEngine.Debug;
 
 namespace Rocket.Unturned.Module
 {
     public class RocketUnturnedModule : IModuleNexus
     {
+        private const string DefaultNugetRepository = "https://api.nuget.org/v3/index.json";
+
         public void initialize()
         {
             //Thank you Unturned for providing a very old Newtonsoft.Json...we better should load our own one
             LoadAssembly("Newtonsoft.Json.dll");
 
 #if NUGET_BOOTSTRAP
+            var assemblyLocation = typeof(RocketUnturnedModule).Assembly.Location;
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+
+            foreach (var file in Directory.GetFiles(assemblyDirectory))
+            {
+                if (file.EndsWith(".dll"))
+                {
+                    LoadAssembly(file);
+                }
+            }
+
             string rocketDirectory = Path.GetFullPath($"Servers/{Dedicator.serverID}/Rocket/");
             if (!Directory.Exists(rocketDirectory))
             {
                 Directory.CreateDirectory(rocketDirectory);
             }
 
+            Debug.Log("Bootstrapping RocketMod for Unturned...");
+
+            var logger = new UnityLoggerAdapter();
             var bootrapper = new RocketDynamicBootstrapper();
-            var bootTask = bootrapper.BootstrapAsync(rocketDirectory, "Rocket.Unturned");
+            var bootTask = bootrapper.BootstrapAsync(rocketDirectory, "Rocket.Unturned", false, DefaultNugetRepository, logger);
 #else
             LoadAssembly("Rocket.Unturned.dll");           
             LoadAssembly("Rocket.UnityEngine.dll");
@@ -29,7 +48,7 @@ namespace Rocket.Unturned.Module
             var bootTask = runtime.InitAsync();
 #endif
 
-            bootTask.RunSynchronously();
+            bootTask.GetAwaiter().GetResult();
         }
 
         public void shutdown()
@@ -41,6 +60,19 @@ namespace Rocket.Unturned.Module
             //Load the dll from the same directory as this assembly
             var currentPath = Path.GetDirectoryName(typeof(RocketUnturnedModule).Assembly.Location) ?? "";
             Assembly.LoadFrom(Path.Combine(currentPath, dllName));
+        }
+    }
+
+    public class UnityLoggerAdapter : LoggerBase
+    {
+        public override void Log(ILogMessage message)
+        {
+            Debug.Log($"[{message.Level}] [NuGet] {message.Message}");
+        }
+
+        public override async Task LogAsync(ILogMessage message)
+        {
+            Log(message);
         }
     }
 }
