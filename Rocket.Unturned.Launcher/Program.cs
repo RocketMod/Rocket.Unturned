938d90799c7c237217177fa5be31dbb4938a208d
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-03-30 20:01:27 +0300

    .NET Core support for Rocket.Unturned.Launcher

--- Rocket.Unturned.Launcher/Program.cs
+++ Rocket.Unturned.Launcher/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Rocket.Unturned.Launcher
 {
     internal class RocketLauncher
     {
+        public static bool IsLinux
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+
         private static TextReader consoleReader;
 
         public static void Main(string[] args)
@@ -19,7 +29,7 @@ namespace Rocket.Unturned.Launcher
             }
             else
             {
-                foreach (string s in new[] { "Unturned_Headless.x86_64", "Unturned_Headless.x86", "Unturned.x86_64", "Unturned.x86", "Unturned.exe"})
+                foreach (string s in new[] { "Unturned_Headless.x86_64", "Unturned_Headless.x86", "Unturned.x86_64", "Unturned.x86", "Unturned.exe" })
                     if (File.Exists(s))
                     {
                         executableName = s;
@@ -29,6 +39,18 @@ namespace Rocket.Unturned.Launcher
 
             if (string.IsNullOrEmpty(executableName))
                 throw new FileNotFoundException("Could not locate Unturned executable");
+
+            if (IsLinux)
+            {
+                var currentDir = Path.GetFullPath(Environment.CurrentDirectory);
+                var lib64Dir = Path.Combine(currentDir, "lib64");
+
+                var fileName = Path.GetFileNameWithoutExtension(executableName);
+                var pluginsDir = Path.Combine(currentDir, fileName + "_Data", "Plugins", "x86_64");
+
+                executableName = $"LD_LIBRARY_PATH={lib64Dir}:{pluginsDir} " + executableName;
+            }
+
             string arguments = "-nographics -batchmode -silent-crashes -logfile 'Servers/"
                 + instanceName
                 + "/unturned.log' +secureserver/"
@@ -38,9 +60,9 @@ namespace Rocket.Unturned.Launcher
 
             FileSystemWatcher fileSystemWatcher = new FileSystemWatcher(".", consoleOutput);
             fileSystemWatcher.Changed += fileSystemWatcher_Changed;
-            consoleReader = new StreamReader(new FileStream(consoleOutput, FileMode.OpenOrCreate, FileAccess.Read,
-                FileShare.ReadWrite));
+            consoleReader = new StreamReader(new FileStream(consoleOutput, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite));
             fileSystemWatcher.EnableRaisingEvents = true;
+
             Process p = new Process
             {
                 StartInfo = new ProcessStartInfo(executableName, arguments)
